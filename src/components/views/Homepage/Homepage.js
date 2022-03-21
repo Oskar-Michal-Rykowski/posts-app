ac3980a9b1f86822a0185ded85571d288568290d
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2022-03-21 20:45:35 +0100

    Add Bootstrap

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Homepage = () => <h1>HOMEPAGE</h1>;
+export const Homepage = () => {
+  return <h1>HOMEPAGE</h1>;
+};
 
 Homepage.propTypes = {
   children: PropTypes.node,
-  className: PropTypes.string,
 };
