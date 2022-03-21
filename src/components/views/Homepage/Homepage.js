be6809b40ad7b52a382f40f5094e61f8271d546a
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2022-03-21 23:20:07 +0100

    Add main view on Homepage

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Col, Container, Form, Row } from 'react-bootstrap';
+
+import styles from './Homepage.module.scss';
 
 export const Homepage = () => {
-  return <h1>HOMEPAGE</h1>;
+  return (
+    <div className={styles.top}>
+      <img
+        className={styles.image}
+        src={require('../../../images/main-photo.jpg')}
+        alt="main"
+      ></img>
+      <div className={styles.gradient}></div>
+      <Container className={styles.elements}>
+        <Row>
+          <Col>
+            <h1 className={styles.slogan}>
+              What is your
+              <br /> business
+              <br />
+              plan?
+            </h1>
+          </Col>
+          <Col>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 Homepage.propTypes = {
